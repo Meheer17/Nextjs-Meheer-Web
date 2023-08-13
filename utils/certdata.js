e4a65379b419e20f4b37592a82834f3f2d12bacf
Meheer17 <meherr17.j@gmail.com>
2023-08-13 20:44:22 +0530

    Trial - 7

--- utils/certdata.js
+++
@@ -1,6 +0,0 @@
-export default async function datac(){
-    const res = await fetch(`https://meheer.vercel.app/api/certificates`,{ next: { revalidate: 10 } });
-    const post = await res.json();
-    const projects = post.data 
-    return projects
-}
